d489789a37243c11501753cd00292a3b5b728742
laurensKubat <laurensKubat@gmail.com>
2018-11-03 19:35:16 -0400

    added dockerfile

--- calculator.go
+++ calculator.go
@@ -1,1 +1,23 @@
 package payoutscript
+
+import ark "github.com/ArkEcosystem/go-client/client/two"
+
+type ShareCalc struct {
+	share float64
+	delegateID string
+	client *ark.Client
+}
+
+func NewShareCalc(delegateID string, share float64) *ShareCalc {
+	return &ShareCalc{
+		share:share,
+		delegateID:delegateID,
+		client:ark.NewClient(nil),
+	}
+}
+
+func (s ShareCalc) CalculateLatestBlock (delegateID string) {
+
+}
+
+func (s ShareCalc)
