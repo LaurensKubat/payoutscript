2642e7c099bba21112ed9a0b29973feef33117cb
laurensKubat <laurensKubat@gmail.com>
2018-11-09 18:26:11 -0500

    I hope this works now

--- Calculator/calculator.go
+++ Calculator/calculator.go
@@ -1,16 +1,24 @@
 package Calculator
 
 import (
-	"github.com/LaurensKubat/payoutscript/payoutscript"
+	"github.com/LaurensKubat/payoutscript"
 	"github.com/pkg/errors"
+	"time"
 )
 
 type ShareCalc struct {
-	share     float64
-	NewBlocks chan payoutscript.Block
+	share        float64
+	NewBlocks    chan payoutscript.Block
+	currentState State
+}
+
+type State struct {
+	Voters    map[payoutscript.VoterAddress]payoutscript.Voter
+	timestamp time.Time
 }
 
 const NoBlock = "No new block"
+const After = "New state is before current state"
 
 func NewShareCalc(share float64) *ShareCalc {
 	return &ShareCalc{
@@ -18,11 +26,23 @@ func NewShareCalc(share float64) *ShareCalc {
 	}
 }
 
-func (s ShareCalc) NextBlock() (payoutscript.Block, error) {
+func (s *ShareCalc) NextBlock() (payoutscript.Block, error) {
 	select {
 	case block := <-s.NewBlocks:
 		return block, nil
 	default:
 		return payoutscript.Block{}, errors.New(NoBlock)
 	}
+}
+
+func (s *ShareCalc) getTotalState(p payoutscript.BlockProducer) error {
+	voters, timestamp := p.GetTotalState()
+	if s.currentState.timestamp.After(timestamp) {
+		return errors.New(After)
+	}
+	s.currentState.timestamp = timestamp
+	for _, voter := range voters {
+		s.currentState.Voters[voter.Address] = voter
+	}
+	return nil
 }
--- producer.go
+++ producer.go
@@ -1,8 +1,12 @@
 package payoutscript
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 // Producer should be implemented by any producer of blocks.
 type BlockProducer interface {
 	GetBlocks(blockchan chan Block, ctx context.Context)
+	GetTotalState() ([]Voter, time.Time)
 }
