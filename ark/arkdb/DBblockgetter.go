55347cab13e8ee5904c52989f02fc28b33113935
laurensKubat <laurensKubat@gmail.com>
2018-12-09 18:02:42 +0100

    WIP

--- ark/arkdb/DBblockgetter.go
+++ ark/arkdb/DBblockgetter.go
@@ -2,6 +2,7 @@ package arkdb
 
 import (
 	"database/sql"
+	"github.com/LaurensKubat/payoutscript"
 )
 import _ "github.com/lib/pq"
 
@@ -13,14 +14,12 @@ func NewDataScraper(db *sql.DB) *DataScraper {
 	return &DataScraper{db: db}
 }
 
-type Transactions struct {
-	transactions []transaction
-}
+type Transactions []transaction
 
 type transaction struct {
 	id              string
 	amount          int64
-	timestamp       int
+	height          int
 	recipientID     string
 	senderPubKey    string
 	serialized      []byte
@@ -29,38 +28,154 @@ type transaction struct {
 	blockID         string
 }
 
-// TODO add
-func (d *DataScraper) getTransactions() (sql.Result, error) {
-	res, err := d.db.Exec(`
-		SELECT transactions.id, transactions.amount, blocks.timestamp, transactions.recipient_id,
+// get all transactions by anyone who ever voted on the delegate
+func (d *DataScraper) getTransactions(voteSerialized string) (Transactions, error) {
+	rows, err := d.db.Query(`
+		SELECT transactions.id, transactions.amount, blocks.height, transactions.recipient_id,
 		transactions.sender_public_key, transanctions.serialized, transactions.type, transactions.fee, transactions.block_id
 		FROM transactions 
 		  INNER JOIN blocks ON transactions.block_id = blocks.id 
-			WHERE transactions.type = 3 AND //
-	`)
+			WHERE transactions.recipient_id IN (
+			  SELECT transactions.recipient_id 
+			  FROM transactions 
+			  WHERE transactions.serialized LIKE '%?%'
+			  )
+	`, voteSerialized)
 
-	return res, err
+	if err != nil {
+		return Transactions{}, err
+	}
+
+	transactions := Transactions{}
+	for rows.Next() {
+		transaction := transaction{}
+		err := rows.Scan(&transaction.id, &transaction.amount, &transaction.height, &transaction.recipientID,
+			&transaction.senderPubKey, &transaction.serialized, &transaction.transactiontype, &transaction.fee,
+			&transaction.blockID)
+		if err != nil {
+			return nil, err
+		}
+		transactions = append(transactions, transaction)
+	}
+	return transactions, nil
 }
 
-func (d *DataScraper) getLastOutTransactions(pubKey string) (sql.Result, error) {
-	res, err := d.db.Exec(`
-	SELECT 
-	transaction.recipient_id, transactions.timestamp, blocks.height, transactions.id, transaction.amount, transaction.fee
-	FROM transaction
-	  INNER JOIN blocks
- 	  ON (blocks.id = transaction.block_id),
-	(SELECT 
-	 MAX(transaction.timestamp) AS max_timestamp,
-	 transaction.recipient_id
-	 FROM transactions AS ts
-	 WHERE transactions.sender_public_key = ?
-	 GROUP BY transactions.sender_public_key) AS maxresults
+type ForgedBlocks []ForgedBlock
 
-	WHERE transactions.recipient_id = maxresults.recipient_id
-	AND transactions.timestamp = maxresults.max_timestamp;`, pubKey)
-	return res, err
+type ForgedBlock struct {
+	Height   int64
+	TotalFee int64
+	Reward   int64
+	PubKey   string
 }
 
-func (d *DataScraper)
+// Select all blocks forged by the delegate and the corresponding fees and rewards
+func (d *DataScraper) getForgedBlocks(pubKey string) (ForgedBlocks, error) {
+	rows, err := d.db.Query(`
+	SELECT blocks.height, blocks.total_fee, blocks.reward, generator_public_key
+	FROM blocks
+	WHERE generator_public_key = ?
+	ORDER BY blocks.height DESC`, pubKey)
 
+	if err != nil {
+		return ForgedBlocks{}, err
+	}
+	blocks := ForgedBlocks{}
+	for rows.Next() {
+		block := ForgedBlock{}
+		err := rows.Scan(&block.Height, &block.TotalFee, &block.Reward, &block.PubKey)
+		if err != nil {
+			return nil, err
+		}
+		blocks = append(blocks, block)
+	}
+	return blocks, nil
+}
 
+type voter struct {
+	address     string
+	pubKey      string
+	voteBalance int64
+	vote        string
+}
+
+type Voters []voter
+
+// get all voters who are voters at the moment of running the payout script
+func (d *DataScraper) getCurrentVoters(pubKey string) (Voters, error) {
+	rows, err := d.db.Query(`
+	SELECT wallets.address, wallets.public_key, wallets.vote_balance, wallets.vote
+	FROM wallets
+	WHERE wallets.vote = ?`, pubKey)
+
+	if err != nil {
+		return nil, err
+	}
+	voters := Voters{}
+	for rows.Next() {
+		voter := voter{}
+		err := rows.Scan(&voter.address, &voter.pubKey, &voter.voteBalance, &voter.vote)
+		if err != nil {
+			return nil, err
+		}
+		voters = append(voters, voter)
+	}
+	return voters, nil
+
+}
+
+// TODO get a list of all transactions ordered by voter and if those voters have forged any blocks, look up those blocks
+// and add their reward to the total balance of that voter
+type state struct {
+	transaction     int
+	amount          int64
+	height          int
+	recipientID     string
+	senderPublicKey string
+	serialized      []byte
+	transactionType int
+	transactionFee  int64
+	blockID         int64
+	producedBlocks  int64
+}
+
+type States []state
+
+func (d *DataScraper) getTotalState(serialized string) (States, error) {
+	rows, err := d.db.Query(`
+	SELECT transactions.id, transactions.amount, blocks.height, transactions.recipient_id,
+		transactions.sender_public_key, transanctions.serialized, transactions.type, transactions.fee,
+	       transactions.block_id, wallets.produced_blocks
+		FROM transactions 
+		  INNER JOIN blocks 
+		    ON transactions.block_id = blocks.id
+		  INNER JOIN wallets 
+		    ON transactions.recipient_id = wallets.address
+		WHERE transactions.recipient_id IN (
+		  SELECT transactions.recipient_id 
+			FROM transactions 
+			  WHERE transactions.serialized LIKE '%?%'
+			  ) `, serialized)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var states States
+	for rows.Next() {
+		state := state{}
+		err := rows.Scan(&state.transaction, &state.amount, &state.height, &state.recipientID, &state.senderPublicKey,
+			&state.serialized, &state.transactionType, &state.transactionFee, &state.blockID, &state.producedBlocks)
+		if err != nil {
+			return nil, err
+		}
+		states = append(states, state)
+	}
+	return states, nil
+}
+
+func (d *DataScraper) StatesToBlocks(states States) []payoutscript.Block {
+	for _, state := range states {
+
+	}
+}
