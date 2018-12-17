d16643844c4f8151bd0b8252fa15d8c53548a687
laurensKubat <laurensKubat@gmail.com>
2018-12-17 21:55:32 +0100

    WIP

--- ark/blockBuilder.go
+++ ark/blockBuilder.go
@@ -6,14 +6,14 @@ import (
 	"github.com/LaurensKubat/payoutscript"
 )
 
-
 // current pagination limit as defined in https://github.com/ArkEcosystem/core/tree/develop/packages/core-api
 const Limit = 100
+
 //the timestamp of DD, which decides the payout percentage for is
 const DDOriginTS = int32(16247647)
 
 type BlockBuilder struct {
-	client   *ark.Client
+	client *ark.Client
 }
 
 func (b *BlockBuilder) getVoterPercentage(ts ark.Timestamp) int64 {
@@ -23,12 +23,12 @@ func (b *BlockBuilder) getVoterPercentage(ts ark.Timestamp) int64 {
 	return 95
 }
 
-func (b *BlockBuilder)getCurrentVoters(delegatePubKey string) (map[payoutscript.VoterAddress]payoutscript.Voter, error) {
-	Voters :=  make(map[payoutscript.VoterAddress]payoutscript.Voter)
+func (b *BlockBuilder) getCurrentVoters(delegatePubKey string) (map[payoutscript.VoterAddress]payoutscript.Voter, error) {
+	Voters := make(map[payoutscript.VoterAddress]payoutscript.Voter)
 	total := 2
 	for page := 1; page <= int(total); page++ {
 		res, _, err := b.client.Delegates.Voters(context.Background(), delegatePubKey,
-			&ark.Pagination{Limit: Limit, Page:page})
+			&ark.Pagination{Limit: Limit, Page: page})
 		if err != nil {
 			return nil, err
 		}
@@ -36,8 +36,8 @@ func (b *BlockBuilder)getCurrentVoters(delegatePubKey string) (map[payoutscript.
 		for _, voter := range res.Data {
 			newVoter := payoutscript.Voter{
 				Address: payoutscript.VoterAddress(voter.Address),
-				PubKey: voter.PublicKey,
-				Balance: voter.Balance
+				PubKey:  voter.PublicKey,
+				Balance: voter.Balance,
 			}
 			Voters[payoutscript.VoterAddress(voter.Address)] = newVoter
 		}
